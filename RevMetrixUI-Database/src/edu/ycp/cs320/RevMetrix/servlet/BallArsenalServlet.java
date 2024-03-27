2c65fc5c2e364e8f6c4fe8834fda2380bdc41560
emmetl913 <elars1002@gmail.com>
2024-03-26 22:22:34 -0400

    Added check for account in the BallArsenalServlet

--- RevMetrixUI-Database/src/edu/ycp/cs320/RevMetrix/servlet/BallArsenalServlet.java
+++ RevMetrixUI-Database/src/edu/ycp/cs320/RevMetrix/servlet/BallArsenalServlet.java
@@ -25,9 +25,9 @@ public class BallArsenalServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		
-		/*if(!AccountServlet.validLogin()) {
+		if(!AccountServlet.validLogin()) {
             req.getRequestDispatcher("/_view/logIn.jsp").forward(req, resp);
-        }*/
+        }
 		
 		System.out.println("BallArsenal Servlet: doGet");	
 		
