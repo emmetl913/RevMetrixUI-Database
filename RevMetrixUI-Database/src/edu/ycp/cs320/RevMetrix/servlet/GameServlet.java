147197b4315e8aad7caba714bfabbd23e1d2dbdc
bwoodward523 <bwoodward@ycp.edu>
2024-03-26 22:18:26 -0400

    Discarded line

--- RevMetrixUI-Database/src/edu/ycp/cs320/RevMetrix/servlet/GameServlet.java
+++ RevMetrixUI-Database/src/edu/ycp/cs320/RevMetrix/servlet/GameServlet.java
@@ -10,7 +10,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import edu.ycp.cs320.RevMetrix.model.Game;
-import edu.ycp.cs320.RevMetrix.servlet.AccountServlet;
 
 
 public class GameServlet extends HttpServlet {
